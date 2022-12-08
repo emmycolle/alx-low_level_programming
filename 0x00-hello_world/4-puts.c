e7a78d6b0f7b51db4e6e6e942aa8044f8e9bf9c0
Enabulele <worldemmycollege@gmail.com>
2022-12-08 15:17:28 -0800

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
