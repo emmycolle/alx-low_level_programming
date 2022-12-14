25f1b6d6f5df09593103c832fb0cca4f96be1c92
Enabulele <worldemmycollege@gmail.com>
2022-12-14 05:36:39 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
