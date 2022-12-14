e0405b066b9ec8e207492905538f148575f74246
Enabulele <worldemmycollege@gmail.com>
2022-12-14 15:37:13 -0800

    Putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,25 +3,22 @@
 
 /**
  * print_to_98 - prints all numbers to 98
- * @n: starting integer
+ * @n: the starting number
  */
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		while (n < 98)
-		{
-			printf("%d, ", n);
-			n++;
-		}
-	}
-	else if (n > 98)
+	if (n >= 98)
 	{
 		while (n > 98)
-		{
-			printf("%d, ", n);
-			n--;
-		}
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	printf("98\n");
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
+
+
