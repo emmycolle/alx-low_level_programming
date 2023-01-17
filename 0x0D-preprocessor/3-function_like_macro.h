70cacb20e51bcd794bd93534293b33ec5029c575
Enabulele <worldemmycollege@gmail.com>
2023-01-17 03:11:00 -0800

    Macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#define ABS(num) ((num) > (-1) ? (num) : (num * -1))
+#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
 #endif /* !FUNCTION_LIKE_MACRO_H */
