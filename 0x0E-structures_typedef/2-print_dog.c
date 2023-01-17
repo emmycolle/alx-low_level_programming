fa787f701bfec94887e577f973eb57fe77ed1d36
Enabulele <worldemmycollege@gmail.com>
2023-01-17 14:05:10 -0800

    Dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,5 @@ void print_dog(struct dog *d)
 
 	printf("Name: %s\n", d->name);
 	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
-
-	return (0);
+	printf("Owner: %s\n", d->owner)
 }
